9da7af9d683609348bd1490ed9d787a5161cd1a5
Monika <monika.m.williams@gmail.com>
2018-03-28 14:08:39 -0400

    added user stats, hit or miss alert, and changed the invaid nuber prompt to an alert

--- battleship.js
+++ battleship.js
@@ -7,15 +7,19 @@ let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
+
 while (isSunk != true) {
 	guess = prompt("Please type in a number between 0 and 6!")
 	if (guess > 6 || guess < 0) {
-		guess = prompt("Please type in a valid number between 0 and 6")
+		alert("Please type in a valid number between 0 and 6")
 	} else {
 		guesses += 1;
 	}
 	if (guess == location1 || guess == location2 || guess == location3) {
+		alert("Hit!")
 		hits += 1;
+	} else {
+		alert("Miss :(")
 	}
 	if (hits == 3) {
 		isSunk = true;
@@ -23,3 +27,6 @@ while (isSunk != true) {
 
 	}
 }
+let stats = "You took " + guesses + " guesses to sink the battleship, " +
+		"which means your shooting accuracy was " + (3/guesses);
+alert(stats);
