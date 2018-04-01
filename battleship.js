58219400ad052c88addc573a110833a34fd528ac
Monika <monika.m.williams@gmail.com>
2018-03-31 22:43:58 -0400

    added styling and made battle ship game more dynamic

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let randomLoc= Math.floor(Math.random() * 5);
+let location1 = randomLoc;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
 
 let guess;
 let hits = 0;
@@ -9,24 +10,24 @@ let isSunk = false;
 
 
 while (isSunk != true) {
-	guess = prompt("Please type in a number between 0 and 6!")
-	if (guess > 6 || guess < 0) {
-		alert("Please type in a valid number between 0 and 6")
+	guess = prompt("Please type in a number between 0 and 6!");
+ if (guess > 6 || guess < 0) {
+		alert("Please type in a valid number between 0 and 6");
 	} else {
 		guesses += 1;
-	}
-	if (guess == location1 || guess == location2 || guess == location3) {
-		alert("Hit!")
-		hits += 1;
-	} else {
-		alert("Miss :(")
-	}
-	if (hits == 3) {
-		isSunk = true;
-		alert("You sunk my Battleship!")
-
+	
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert("Hit!");
+			hits += 1;
+			if (hits == 3) {
+				isSunk = true;
+				alert("You sunk my Battleship!");
+			}
+		} else {
+			alert("Miss :(")
+		}
 	}
 }
 let stats = "You took " + guesses + " guesses to sink the battleship, " +
-		"which means your shooting accuracy was " + (3/guesses);
+	"which means your shooting accuracy was " + (3 / guesses);
 alert(stats);
